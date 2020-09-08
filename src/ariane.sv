3c7f1317d3e8c257121f0c87a6f20057da17f414
Eugene Feinberg <eugene@recogni.com>
2020-09-08 12:11:00 -0700

    Remove instr_tracer temporarily

--- src/ariane.sv
+++ src/ariane.sv
@@ -666,25 +666,6 @@ module ariane #(
   `endif
   // pragma translate_on
 
-  // -------------------
-  // Instruction Tracer
-  // -------------------
-
-  // Instruction trace port (used for FireSim)
-`ifdef FIRESIM_TRACE
-  for (genvar i = 0; i < NR_COMMIT_PORTS; i++) begin : gen_tp_connect
-    assign trace_o[i].clock = clk_i;
-    assign trace_o[i].reset = rst_ni;
-    assign trace_o[i].valid = commit_ack[i] & ~commit_instr_id_commit[i].ex.valid;
-    assign trace_o[i].iaddr = commit_instr_id_commit[i].pc;
-    assign trace_o[i].insn = commit_instr_id_commit[i].ex.tval[31:0];
-    assign trace_o[i].priv = priv_lvl;
-    assign trace_o[i].exception = commit_ack[i] & commit_instr_id_commit[i].ex.valid & ~commit_instr_id_commit[i].ex.cause[63];
-    assign trace_o[i].interrupt = commit_ack[i] & commit_instr_id_commit[i].ex.valid & commit_instr_id_commit[i].ex.cause[63];
-    assign trace_o[i].cause = commit_instr_id_commit[i].ex.cause;
-    assign trace_o[i].tval = commit_instr_id_commit[i].ex.tval[31:0];
-  end
-`endif
 
   //pragma translate_off
 `ifdef PITON_ARIANE
@@ -731,96 +712,5 @@ module ariane #(
     .idx_o   (              )
   );
 `endif // PITON_ARIANE
-
-`ifndef VERILATOR
-  instr_tracer_if tracer_if (clk_i);
-  // assign instruction tracer interface
-  // control signals
-  assign tracer_if.rstn              = rst_ni;
-  assign tracer_if.flush_unissued    = flush_unissued_instr_ctrl_id;
-  assign tracer_if.flush             = flush_ctrl_ex;
-  // fetch
-  assign tracer_if.instruction       = id_stage_i.fetch_entry_i.instruction;
-  assign tracer_if.fetch_valid       = id_stage_i.fetch_entry_valid_i;
-  assign tracer_if.fetch_ack         = id_stage_i.fetch_entry_ready_o;
-  // Issue
-  assign tracer_if.issue_ack         = issue_stage_i.i_scoreboard.issue_ack_i;
-  assign tracer_if.issue_sbe         = issue_stage_i.i_scoreboard.issue_instr_o;
-  // write-back
-  assign tracer_if.waddr             = waddr_commit_id;
-  assign tracer_if.wdata             = wdata_commit_id;
-  assign tracer_if.we_gpr            = we_gpr_commit_id;
-  assign tracer_if.we_fpr            = we_fpr_commit_id;
-  // commit
-  assign tracer_if.commit_instr      = commit_instr_id_commit;
-  assign tracer_if.commit_ack        = commit_ack;
-  // branch predict
-  assign tracer_if.resolve_branch    = resolved_branch;
-  // address translation
-  // stores
-  assign tracer_if.st_valid          = ex_stage_i.lsu_i.i_store_unit.store_buffer_i.valid_i;
-  assign tracer_if.st_paddr          = ex_stage_i.lsu_i.i_store_unit.store_buffer_i.paddr_i;
-  // loads
-  assign tracer_if.ld_valid          = ex_stage_i.lsu_i.i_load_unit.req_port_o.tag_valid;
-  assign tracer_if.ld_kill           = ex_stage_i.lsu_i.i_load_unit.req_port_o.kill_req;
-  assign tracer_if.ld_paddr          = ex_stage_i.lsu_i.i_load_unit.paddr_i;
-  // exceptions
-  assign tracer_if.exception         = commit_stage_i.exception_o;
-  // assign current privilege level
-  assign tracer_if.priv_lvl          = priv_lvl;
-  assign tracer_if.debug_mode        = debug_mode;
-
-  instr_tracer instr_tracer_i (
-    .tracer_if(tracer_if),
-    .hart_id_i
-  );
-
-// mock tracer for Verilator, to be used with spike-dasm
-`else
-
-  int f;
-  logic [63:0] cycles;
-
-  initial begin
-    f = $fopen("trace_hart_00.dasm", "w");
-  end
-
-  always_ff @(posedge clk_i or negedge rst_ni) begin
-    if (~rst_ni) begin
-      cycles <= 0;
-    end else begin
-      string mode = "";
-      if (debug_mode) mode = "D";
-      else begin
-        case (priv_lvl)
-        riscv::PRIV_LVL_M: mode = "M";
-        riscv::PRIV_LVL_S: mode = "S";
-        riscv::PRIV_LVL_U: mode = "U";
-        endcase
-      end
-      for (int i = 0; i < NR_COMMIT_PORTS; i++) begin
-        if (commit_ack[i] && !commit_instr_id_commit[i].ex.valid) begin
-          $fwrite(f, "%d 0x%0h %s (0x%h) DASM(%h)\n", cycles, commit_instr_id_commit[i].pc, mode, commit_instr_id_commit[i].ex.tval[31:0], commit_instr_id_commit[i].ex.tval[31:0]);
-        end else if (commit_ack[i] && commit_instr_id_commit[i].ex.valid) begin
-          if (commit_instr_id_commit[i].ex.cause == 2) begin
-            $fwrite(f, "Exception Cause: Illegal Instructions, DASM(%h) PC=%h\n", commit_instr_id_commit[i].ex.tval[31:0], commit_instr_id_commit[i].pc);
-          end else begin
-            if (debug_mode) begin
-              $fwrite(f, "%d 0x%0h %s (0x%h) DASM(%h)\n", cycles, commit_instr_id_commit[i].pc, mode, commit_instr_id_commit[i].ex.tval[31:0], commit_instr_id_commit[i].ex.tval[31:0]);
-            end else begin
-              $fwrite(f, "Exception Cause: %5d, DASM(%h) PC=%h\n", commit_instr_id_commit[i].ex.cause, commit_instr_id_commit[i].ex.tval[31:0], commit_instr_id_commit[i].pc);
-            end
-          end
-        end
-      end
-        cycles <= cycles + 1;
-    end
-  end
-
-  final begin
-    $fclose(f);
-  end
-`endif // VERILATOR
-//pragma translate_on
 
 endmodule // ariane
